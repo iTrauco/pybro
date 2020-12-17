56e88b6e5e038ed9f888cd5727fdfdbb0cddd667
iTrauco <chris@trau.co>
2020-12-16 21:20:22 -0500

    initial commit - gui browser automation

---
+++ script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import webbrowser
+
+webbrowser.open_new('https://pandora.lightning.force.com/')
+webbrowser.open_new('https://www.clearslide.com/me')
+webbrowser.open_new('https://admanager.google.com/4204#home')
+webbrowser.open_new('https://admin.google.com')
+webbrowser.open_new('https://www.linkedin.com/sales/login')
+
+
