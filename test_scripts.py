ffea54ebb745050212a105554e476d5c16b98c80
iTrauco <chris@trau.co>
2020-12-18 17:45:47 -0500

    initial commit / defined project readme

---
+++ test_scripts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import webbrowser
+
+webbrowser.open_new('')
+webbrowser.open_new('')
+webbrowser.open_new('')
+webbrowser.open_new('')
+webbrowser.open_new('')
+webbrowser.open_new('')
+
+
